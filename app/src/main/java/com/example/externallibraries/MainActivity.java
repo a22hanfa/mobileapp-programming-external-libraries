1e03aa67ac87757aa6b605754f543d8a2bd4a67c
Hannes <12446977+glazedegg@users.noreply.github.com>
2023-05-12 22:36:15 +0200

    - Added the ability to load and display image from URL in ImageView

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.externallibraries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.my_image_view);
+
+        String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg";
+
+        Glide.with(this)
+                .load(imageUrl)
+                .into(imageView);
+
     }
 }
